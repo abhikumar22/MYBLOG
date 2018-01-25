ecd5292dcbe7fa56e9bcc607d6cf37a2ba2f08b0
Abhishek kumar <abhi.ckp1002@gmail.com>
2018-01-26 01:26:20 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = '/'
 
